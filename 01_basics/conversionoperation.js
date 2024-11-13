5ed786e06f24bcedbdd5c315f2c57dd1276588a5
Fireball9936675762 <117505245+Fireball9936675762@users.noreply.github.com>
2024-11-13 04:34:20 +0000

    conversionoperations

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,15 @@
+// let score = 56;
+let score = 56;
+
+// console.log(typeof(score));
+let valueinNumber = Number(score);
+// console.log(valueinNumber);
+// console.log(typeof valueinNumber);
+
+let valueinString = String(score);
+// console.log(valueinString);
+// console.log(typeof valueinString);
+
+let boolVaalue = 2;
+let valueInBool = Boolean(boolVaalue);
+console.log(typeof valueInBool);
