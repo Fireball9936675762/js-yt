38736d09a13100d13c72259d94ce9048ccc08b51
Fireball9936675762 <117505245+Fireball9936675762@users.noreply.github.com>
2024-11-17 05:45:44 +0000

    array_01

--- 2_basics/1_arrays.js
+++ 2_basics/1_arrays.js
@@ -1,2 +1,23 @@
-let array=[1,3,8]
-console.log(array);
+let array=[1,3,8, 7,1]
+// console.log(array);
+let array2=[8,9,5]
+// console.log(array.concat(array2));
+
+// console.log(array.push(5));
+// console.log(array.pop());
+// console.log(array.unshift(6));
+// console.log(array.shift());
+
+// console.log(array2.includes(4));
+// console.log(array2.includes(9));
+// console.log(array2.indexOf(9));
+// console.log(array2.indexOf(4));
+
+let array3= array2.join()
+// console.log(array3);
+// console.log(typeof array3);
+
+//slice, splice
+
+console.log("A ", array);
+let newArray= splice(array)
