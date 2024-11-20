57d0c6aeac886e41044302f2af043f5698725279
Fireball9936675762 <117505245+Fireball9936675762@users.noreply.github.com>
2024-11-20 03:15:52 +0000

    this and arrow function

--- 3_basics/4_arrow.js
+++ 3_basics/4_arrow.js
@@ -19,9 +19,9 @@ const user={
         
     }
 }
-user.welcomeMessage();
-user.name="Maurya"
-user.welcomeMessage();
+// user.welcomeMessage();
+// user.name="Maurya"
+// user.welcomeMessage();
 
 
 const user1={
@@ -32,5 +32,31 @@ const user1={
         
     }
 }
-console.log(this);          //*********importent******** */
+// console.log(this);          //*********importent******** */
 //in node environment this line will print empty object bcz there is no global contex but in case of browser environment it will return "WINDOW" bcz window is global object in browser
+
+function chai() {
+    console.log(this);
+    // let username= "Gaurav"
+    // console.log(this.username);      //this mathod can only be used in case of object
+}
+// chai()
+
+//*****************Arrow function************************* */
+
+const coffee=()=>{
+    
+    // let username= "Gaurav"
+    // console.log(this.username); 
+    console.log(this);
+    
+}
+// coffee()
+
+
+const add =(num1,num2)=> (num1+num2)                //implicite return
+console.log(add(4,5));
+
+const print =()=> ({username:"Gaurav"} )               //implicite return
+console.log(print());
+
