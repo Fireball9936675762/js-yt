8a9319f66d96b7887f5b6f6fb6475fa247d9748c
Fireball9936675762 <117505245+Fireball9936675762@users.noreply.github.com>
2025-01-02 10:59:59 +0000

    strings

--- 01_basics/4_strings.js
+++ 01_basics/4_strings.js
@@ -2,7 +2,7 @@ const firstname="Gaurav"
 const laastname="Maurya"
 
 // console.log(firstname + laastname);
-console.log(`hello my name is ${firstname} ${laastname}`);
+// console.log(`hello my name is ${firstname} ${laastname}`);
 
 const myName= new String("GauravMaurya")
 // it is used to create object which can be more understood in browser consle
@@ -16,7 +16,8 @@ console.log(myName.slice(0,6));     //-ve value is also acceptable
 console.log(myName.toUpperCase());
 console.log(myName.charAt(5));
 console.log(myName.indexOf("r"));
-console.log(myName.split(0,6));
+console.log(myName.split());
+console.log(myName.split(''));
 console.log(myName.substring(0,4));
 
 let newstring ="    Gaurav   "      //confussion: newstring is not object yet we apply method on it
